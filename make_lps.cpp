a96638d8a7765e9ec8061833b5e6967b907ea3c0
say-1104 <okazkai.sei.a6@elms.hokudai.ac.jp>
2024-05-09 15:07:18 +0900

    add

--- make_lps.cpp
+++ make_lps.cpp
@@ -1,24 +1,20 @@
 #include "make_lps.hpp"
 
 int main(int argc, char *argv[]){
+	double trans = 0.4, gene = 0.4;
 	string lpsname = "pcmDC";
-	string filename = "pcmDC.lps";
 
-	ofstream ofs;
-	ofs.open( "pcmDC.lps");
+	ofs.open("pcmDC.lps");
 
 	if(! ofs) {
 		cerr << "File open error !" << endl;
 		exit(1);
 	}
 
+	cout << "writing " << lpsname << "..." << endl;
 
 
-	cout << "writing " << filename << "..." << endl;
-
-	for (int i = 0; i<10; i++){
-		ofs << i << endl;
-	}
+	Filename(lpsname);
 	ofs.close();
 	return 0;
 }
--- make_lps.hpp
+++ make_lps.hpp
@@ -5,6 +5,22 @@
 #include <cstdlib>
 using namespace std;
 
+ofstream ofs;
+
 #define PI		    3.141592653589793
 
 #define ZERO -1.0e-6
+
+void Trans_Gene(double trans, double gene){
+	ofs << "TransitionFactor" << endl;
+	ofs << trans << " 0\n" << endl;
+	ofs << "Generate" << endl;
+	ofs << gene << endl;
+}
+
+void Filename(string lpsname){
+	ofs << "saveMesh" << endl;
+	ofs << lpsname << endl;
+	ofs << "exportMesh" << endl;
+	ofs << lpsname << endl;
+}
