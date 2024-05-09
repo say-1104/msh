040280d0c3dc06e385278b7f7877151224ac963e
say-1104 <okazkai.sei.a6@elms.hokudai.ac.jp>
2024-05-09 16:11:59 +0900

    commit

--- make_lps.cpp
+++ make_lps.cpp
@@ -3,18 +3,22 @@
 int main(int argc, char *argv[]){
 	double trans = 0.4, gene = 0.4;
 	string lpsname = "pcmDC";
+	Function func; 
 
 	ofs.open("pcmDC.lps");
-
 	if(! ofs) {
 		cerr << "File open error !" << endl;
 		exit(1);
 	}
-
 	cout << "writing " << lpsname << "..." << endl;
 
+	Point(0.0, 0.0, 0.0, &func);
 
-	Filename(lpsname);
+	Trans_Gene(trans, gene, &func);
+
+	Filename(lpsname, &func);
+
 	ofs.close();
+
 	return 0;
 }
--- make_lps.hpp
+++ make_lps.hpp
@@ -11,16 +11,96 @@ ofstream ofs;
 
 #define ZERO -1.0e-6
 
-void Trans_Gene(double trans, double gene){
+typedef struct _Function {
+    int flag;
+} Function;
+
+void Start_End(Function *func, int num){
+	if(func->flag == num) return;
+	else {
+		switch(func->flag){
+			case 1:
+				ofs << "Point END\n" << endl;
+				break;
+			case 2:
+				ofs << "Line END\n" << endl;
+				break;
+			case 3:
+				ofs << "Surface END\n" << endl;
+				break;
+			case 4:
+				ofs << "Volume END\n" << endl;
+				break;
+			case 5:
+				ofs << "Copy END\n" << endl;
+				break;
+			case 6:
+				ofs << "Rotation END\n" << endl;
+				break;
+			case 7:
+				ofs << "Material END\n" << endl;
+				break;
+			case 8:
+				ofs << "Unstr_mesh END\n" << endl;
+				break;
+			case 9:
+				ofs << "Str_mesh END\n" << endl;
+				break;
+		}
+
+		switch(num){
+			case 1:
+				ofs << "Point" << endl;
+				break;
+			case 2:
+				ofs << "Line" << endl;
+				break;
+			case 3:
+				ofs << "Surface" << endl;
+				break;
+			case 4:
+				ofs << "Volume" << endl;
+				break;
+			case 5:
+				ofs << "Copy" << endl;
+				break;
+			case 6:
+				ofs << "Rotation" << endl;
+				break;
+			case 7:
+				ofs << "Material" << endl;
+				break;
+			case 8:
+				ofs << "Unstr_mesh" << endl;
+				break;
+			case 9:
+				ofs << "Str_mesh" << endl;
+				break;
+		}
+		func->flag = num;
+	}
+}
+
+
+void Point(double x, double y, double z, Function *func){
+	Start_End(func, 1);
+	ofs << x << " " << y << " " << z << endl;
+}
+
+void Trans_Gene(double trans, double gene, Function *func){
+	Start_End(func, 0);
 	ofs << "TransitionFactor" << endl;
 	ofs << trans << " 0\n" << endl;
 	ofs << "Generate" << endl;
-	ofs << gene << endl;
+	ofs << gene << "\n" << endl;
 }
 
-void Filename(string lpsname){
+void Filename(string lpsname, Function *func){
+	Start_End(func, 0);
 	ofs << "saveMesh" << endl;
 	ofs << lpsname << endl;
 	ofs << "exportMesh" << endl;
 	ofs << lpsname << endl;
 }
+
+
