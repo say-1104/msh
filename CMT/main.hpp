7847212e514eff257ae86cedb00bcc3ac6761711
say-1104 <okazkai.sei.a6@elms.hokudai.ac.jp>
2024-05-30 13:37:35 +0900

    lab

--- CMT/main.hpp
+++ CMT/main.hpp
@@ -7,20 +7,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <complex>
 
 constexpr std::complex<double> cj(0.0, 1.0);
-#ifndef PI
-#define PI 3.1415926535897
-#endif
-#define Z0 376.73036
-
-#define ALLOCATION(a,c,n)  if ((a = (c *)malloc((n)*sizeof(c))) == NULL) {\
-								fprintf(stderr, "can't allocate memory\n");\
-								exit(0);\
-							}
 
 #endif
