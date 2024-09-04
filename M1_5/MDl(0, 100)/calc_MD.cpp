64dc149c48257385ad18bc1172c435fd78c5618a
say-1104 <okazaki.sei.a6@elms.hokudai.ac.jp>
2024-09-04 14:29:32 +0900

    note

---
+++ M1_5/MDl(0, 100)/calc_MD.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    int N_wl = 5;
+    double MDlambda = 0.0;
+    double wish = 1.0;
+    for (int i=0; i<N_wl; i++){
+            double l, t;
+            cin >> l >> t;
+            MDlambda = max(MDlambda, abs(t-wish));
+            //cout << MDlambda << endl;
+    }
+    wish = 0.0;
+    for (int i=0; i<N_wl; i++){
+            double l, t;
+            cin >> l >> t;
+            MDlambda = max(MDlambda, abs(t-wish));
+            //cout << MDlambda << endl;
+    }
+
+    fstream ofs;
+    ofs.open("MDlambda", std::ios::out);
+	if(! ofs) {
+		std::cerr << "File(MDlambda) open error !" << std::endl;
+		exit(1);
+    }
+
+    ofs << fixed << setprecision(10);
+    ofs << MDlambda << endl;
+    ofs.close();
+    return 0;
+}
--- M1_5/calc2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./cmt -pcm1 
